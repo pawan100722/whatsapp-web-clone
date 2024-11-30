f876ad918d74942b35e2500d4720bdb16513153d
pawan100722 <pawan100722@gmail.com>
2024-11-30 17:04:43 +0530

    added icons for chatlist header

--- src/Components/ChatList.tsx
+++ src/Components/ChatList.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import '../CSS/ChatList.css';
+import  NewChatIcon  from '../Icons/NewChatIcon.tsx';
+import MenuIcon from '../Icons/MenuIcon.tsx';
 
 export const ChatList=()=>{
-  return <div className='chat-list-container'></div>
+  return <div className='chat-list-container'>
+    <div className="header-container">
+      <div className="heading">
+        <h1>Chats</h1>
+      </div>
+      <div className="new-chat icon">
+        <NewChatIcon/>
+      </div>
+      <div className="menu icon">
+        <MenuIcon/>
+      </div>
+    </div>
+  </div>
 }
